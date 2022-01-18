64de2de66797961013c34917a2dc8570d41bd8fa
Hansaem Park <hansaempark@sunssc.com>
2022-01-18 09:44:01 +0900

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import Intro from "./components/Intro";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div`
+    height: 100vh;
+`
+
+const App = () => {
+    return (
+        <Container>
+            <Navbar />
+            <Intro />
+        </Container>
+    )
+}
+
+export default App;
---
+++ src/components/Intro.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: calc(100vh - 50px);
+    background-color: orange;
+`;
+
+const Intro = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Intro
